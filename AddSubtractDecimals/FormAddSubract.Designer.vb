710fccb59a2c4f737ce6b90dc4111f61aec80ac2
Thomas Downes <ThomasD@CardIntegrators.com>
2019-06-25 06:43:35 -0700

    Added a new project for Miles and Ruben, to allow them to navigate Reported Number = 2 ^64 (has 20 digits) minus Printed Number.

---
+++ AddSubtractDecimals/FormAddSubract.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormAddSubract
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextSummand1 = New System.Windows.Forms.TextBox()
+        Me.txtSummand2 = New System.Windows.Forms.TextBox()
+        Me.textAnswerAdd = New System.Windows.Forms.TextBox()
+        Me.LabelLeftHdrAdd = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelLeftHdrEqualsAdd = New System.Windows.Forms.Label()
+        Me.PanelAdd = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextAnswerMinus = New System.Windows.Forms.TextBox()
+        Me.LabelLeftHdrEqualsMinus = New System.Windows.Forms.Label()
+        Me.txtMinus1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMinus2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PanelAdd.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextSummand1
+        '
+        Me.TextSummand1.Location = New System.Drawing.Point(106, 17)
+        Me.TextSummand1.Name = "TextSummand1"
+        Me.TextSummand1.Size = New System.Drawing.Size(705, 22)
+        Me.TextSummand1.TabIndex = 0
+        '
+        'txtSummand2
+        '
+        Me.txtSummand2.Location = New System.Drawing.Point(106, 57)
+        Me.txtSummand2.Name = "txtSummand2"
+        Me.txtSummand2.Size = New System.Drawing.Size(705, 22)
+        Me.txtSummand2.TabIndex = 1
+        '
+        'textAnswerAdd
+        '
+        Me.textAnswerAdd.Location = New System.Drawing.Point(106, 114)
+        Me.textAnswerAdd.Name = "textAnswerAdd"
+        Me.textAnswerAdd.Size = New System.Drawing.Size(705, 22)
+        Me.textAnswerAdd.TabIndex = 2
+        '
+        'LabelLeftHdrAdd
+        '
+        Me.LabelLeftHdrAdd.AutoSize = True
+        Me.LabelLeftHdrAdd.Location = New System.Drawing.Point(67, 60)
+        Me.LabelLeftHdrAdd.Name = "LabelLeftHdrAdd"
+        Me.LabelLeftHdrAdd.Size = New System.Drawing.Size(33, 17)
+        Me.LabelLeftHdrAdd.TabIndex = 3
+        Me.LabelLeftHdrAdd.Text = "Add"
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.DimGray
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(67, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(744, 10)
+        Me.Label1.TabIndex = 5
+        '
+        'LabelLeftHdrEqualsAdd
+        '
+        Me.LabelLeftHdrEqualsAdd.AutoSize = True
+        Me.LabelLeftHdrEqualsAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLeftHdrEqualsAdd.ForeColor = System.Drawing.Color.Blue
+        Me.LabelLeftHdrEqualsAdd.Location = New System.Drawing.Point(49, 117)
+        Me.LabelLeftHdrEqualsAdd.Name = "LabelLeftHdrEqualsAdd"
+        Me.LabelLeftHdrEqualsAdd.Size = New System.Drawing.Size(51, 17)
+        Me.LabelLeftHdrEqualsAdd.TabIndex = 6
+        Me.LabelLeftHdrEqualsAdd.Text = "Equals"
+        '
+        'PanelAdd
+        '
+        Me.PanelAdd.BackColor = System.Drawing.Color.Honeydew
+        Me.PanelAdd.Controls.Add(Me.textAnswerAdd)
+        Me.PanelAdd.Controls.Add(Me.LabelLeftHdrEqualsAdd)
+        Me.PanelAdd.Controls.Add(Me.TextSummand1)
+        Me.PanelAdd.Controls.Add(Me.Label1)
+        Me.PanelAdd.Controls.Add(Me.txtSummand2)
+        Me.PanelAdd.Controls.Add(Me.LabelLeftHdrAdd)
+        Me.PanelAdd.Location = New System.Drawing.Point(12, 96)
+        Me.PanelAdd.Name = "PanelAdd"
+        Me.PanelAdd.Size = New System.Drawing.Size(883, 159)
+        Me.PanelAdd.TabIndex = 7
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.MintCream
+        Me.Panel1.Controls.Add(Me.TextAnswerMinus)
+        Me.Panel1.Controls.Add(Me.LabelLeftHdrEqualsMinus)
+        Me.Panel1.Controls.Add(Me.txtMinus1)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.txtMinus2)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(12, 270)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(883, 159)
+        Me.Panel1.TabIndex = 8
+        '
+        'TextAnswerMinus
+        '
+        Me.TextAnswerMinus.Location = New System.Drawing.Point(106, 114)
+        Me.TextAnswerMinus.Name = "TextAnswerMinus"
+        Me.TextAnswerMinus.Size = New System.Drawing.Size(705, 22)
+        Me.TextAnswerMinus.TabIndex = 2
+        '
+        'LabelLeftHdrEqualsMinus
+        '
+        Me.LabelLeftHdrEqualsMinus.AutoSize = True
+        Me.LabelLeftHdrEqualsMinus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLeftHdrEqualsMinus.ForeColor = System.Drawing.Color.Blue
+        Me.LabelLeftHdrEqualsMinus.Location = New System.Drawing.Point(49, 117)
+        Me.LabelLeftHdrEqualsMinus.Name = "LabelLeftHdrEqualsMinus"
+        Me.LabelLeftHdrEqualsMinus.Size = New System.Drawing.Size(51, 17)
+        Me.LabelLeftHdrEqualsMinus.TabIndex = 6
+        Me.LabelLeftHdrEqualsMinus.Text = "Equals"
+        '
+        'txtMinus1
+        '
+        Me.txtMinus1.Location = New System.Drawing.Point(106, 17)
+        Me.txtMinus1.Name = "txtMinus1"
+        Me.txtMinus1.Size = New System.Drawing.Size(705, 22)
+        Me.txtMinus1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.DimGray
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(67, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(744, 10)
+        Me.Label3.TabIndex = 5
+        '
+        'txtMinus2
+        '
+        Me.txtMinus2.Location = New System.Drawing.Point(106, 57)
+        Me.txtMinus2.Name = "txtMinus2"
+        Me.txtMinus2.Size = New System.Drawing.Size(705, 22)
+        Me.txtMinus2.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(55, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Minus"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Add"
+        '
+        'FormAddSubract
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(926, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PanelAdd)
+        Me.Name = "FormAddSubract"
+        Me.Text = "Add or Subtract - Decimal Number of Any Size"
+        Me.PanelAdd.ResumeLayout(False)
+        Me.PanelAdd.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextSummand1 As TextBox
+    Friend WithEvents txtSummand2 As TextBox
+    Friend WithEvents textAnswerAdd As TextBox
+    Friend WithEvents LabelLeftHdrAdd As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelLeftHdrEqualsAdd As Label
+    Friend WithEvents PanelAdd As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextAnswerMinus As TextBox
+    Friend WithEvents LabelLeftHdrEqualsMinus As Label
+    Friend WithEvents txtMinus1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtMinus2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ AddSubtractDecimals/FormAddSubract.vb
@@ -0,0 +1,17 @@
+﻿Public Class FormAddSubract
+
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextSummand1.KeyPress
+
+        e.Handled = Not Char.IsDigit(e.KeyChar)
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub FormAddSubract_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
